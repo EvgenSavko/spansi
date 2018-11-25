fa17abb2ca657550ea7fadc4efd57d2104ba1278
senyaq <senyaq@gmail.com>
2018-11-26 00:21:06 +0200

    add header layout

--- src/component/page-markup/Header.js
+++ src/component/page-markup/Header.js
@@ -5,10 +5,29 @@ class Header extends Component {
   render() {
     return (
       <div className="block_header">
-        <h3>SPANSI</h3>
-        <h4>Sing In</h4>
-        <h4>about</h4>
-        <h3>.|œ0|</h3>
+        <div className="content">
+          <div className="row">
+            <div className="left">
+              <h4>SPANSI</h4>
+            </div>
+            <div className="center">
+              <div>
+                <h5>Sing in</h5>
+              </div>
+              <div>
+                <h5>about</h5>
+              </div>
+            </div>
+            <div className="right">
+              <div>
+                <h4>.|œ0|</h4>
+              </div>
+              <div>
+                <h4>23:22</h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <div className="background_home">
+          <div />
+        </div>
         <h2>Home</h2>
         <p>
           Apod <span> go moon</span>
