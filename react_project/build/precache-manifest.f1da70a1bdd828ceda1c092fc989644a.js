f12791142bf8b7894de9bb87b308acec20941164
senyaq <senyaq@gmail.com>
2019-04-09 22:07:35 +0300

    added build

---
+++ react_project/build/precache-manifest.f1da70a1bdd828ceda1c092fc989644a.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "5efe7b202026eab07265",
+    "url": "/static/css/main.6b0aec3e.chunk.css"
+  },
+  {
+    "revision": "6a4fc59f7ab55dd4785f262296394406",
+    "url": "/static/media/not_photo.6a4fc59f.jpg"
+  },
+  {
+    "revision": "dc58b540c004e3224c91",
+    "url": "/static/js/1.dc58b540.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d132159c6069526c56b0c16135e24873",
+    "url": "/static/media/earth.d132159c.png"
+  },
+  {
+    "revision": "9ea36b2644b9d91cc8db8c9979971e6c",
+    "url": "/static/media/God_Is_An_astronaut_Loss.9ea36b26.ogg"
+  },
+  {
+    "revision": "1acf87f82f4a115ca9e02c68f54109ce",
+    "url": "/static/media/logo1.1acf87f8.png"
+  },
+  {
+    "revision": "648d4ccf76d2fabcda6460e2f4f7b74f",
+    "url": "/static/media/logo_active1.648d4ccf.png"
+  },
+  {
+    "revision": "35a588db144e6a6d5e341629237b8767",
+    "url": "/static/media/mars.35a588db.jpg"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "3b9d30de01428d18e32051b11697a34f",
+    "url": "/static/media/OFL.3b9d30de.txt"
+  },
+  {
+    "revision": "8971d1710cbf4c91bca1b460aec154d7",
+    "url": "/static/media/Poppins-Black.8971d171.ttf"
+  },
+  {
+    "revision": "ad021f07d5d9269d7a7c005e2ad19eae",
+    "url": "/static/media/Poppins-BlackItalic.ad021f07.ttf"
+  },
+  {
+    "revision": "5efe7b202026eab07265",
+    "url": "/static/js/main.5efe7b20.chunk.js"
+  },
+  {
+    "revision": "0952866304f326356a552d9b60a8f06e",
+    "url": "/static/media/Poppins-BoldItalic.09528663.ttf"
+  },
+  {
+    "revision": "0e6906b2b7be194f68b8f7b7252c4f6c",
+    "url": "/static/media/Poppins-ExtraBold.0e6906b2.ttf"
+  },
+  {
+    "revision": "8f1e4e0b5a883ba1035b10f6bd09f19f",
+    "url": "/static/media/Poppins-ExtraBoldItalic.8f1e4e0b.ttf"
+  },
+  {
+    "revision": "f99f9d50a569dbcf72e3084ef1a43208",
+    "url": "/static/media/Poppins-ExtraLight.f99f9d50.ttf"
+  },
+  {
+    "revision": "682c805ca8e4f6d0f66b4f9fbbb83fac",
+    "url": "/static/media/Poppins-ExtraLightItalic.682c805c.ttf"
+  },
+  {
+    "revision": "b7e7ed9f2fd6236ad791a8987d8f478f",
+    "url": "/static/media/Poppins-Italic.b7e7ed9f.ttf"
+  },
+  {
+    "revision": "3352653dedd571bbc490c8be132b38cd",
+    "url": "/static/media/Poppins-Light.3352653d.ttf"
+  },
+  {
+    "revision": "f86967072b9e0d95ccd5587decb10180",
+    "url": "/static/media/Poppins-LightItalic.f8696707.ttf"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "049c11dc561dad0fdd4627417030386b",
+    "url": "/static/media/Poppins-MediumItalic.049c11dc.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "e63b93dfac2600782654e2b87910d681",
+    "url": "/static/media/Poppins-SemiBold.e63b93df.ttf"
+  },
+  {
+    "revision": "e650e72170e79b589af1fbdf46b03676",
+    "url": "/static/media/Poppins-SemiBoldItalic.e650e721.ttf"
+  },
+  {
+    "revision": "735aa7d8e35b63068b9113ea2545f0c3",
+    "url": "/static/media/Poppins-Thin.735aa7d8.ttf"
+  },
+  {
+    "revision": "cffb0ae21d03a32e81d5f2193c4a711d",
+    "url": "/static/media/Poppins-ThinItalic.cffb0ae2.ttf"
+  },
+  {
+    "revision": "d273d63619c9aeaf15cdaf76422c4f87",
+    "url": "/static/media/LICENSE.d273d636.txt"
+  },
+  {
+    "revision": "ec4c9962ba54eb91787aa93d361c10a8",
+    "url": "/static/media/Roboto-Black.ec4c9962.ttf"
+  },
+  {
+    "revision": "50705c5ed1205b63efdbfee941a6b655",
+    "url": "/static/media/Roboto-BlackItalic.50705c5e.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "1eb7a893589ddce89d81cdb22a356660",
+    "url": "/static/media/Roboto-BoldItalic.1eb7a893.ttf"
+  },
+  {
+    "revision": "42bbe4eefcde1297b11dc4b6491e9746",
+    "url": "/static/media/Roboto-Italic.42bbe4ee.ttf"
+  },
+  {
+    "revision": "d1efcd4d126837fe0dcf9b6cf3a00d64",
+    "url": "/static/media/Roboto-LightItalic.d1efcd4d.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "bd19ad60600a1537c00d3b4923a5e5de",
+    "url": "/static/media/Roboto-MediumItalic.bd19ad60.ttf"
+  },
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "0fc25386220a58203994ce45fb4ae570",
+    "url": "/static/media/Roboto-ThinItalic.0fc25386.ttf"
+  },
+  {
+    "revision": "ddb438e05ae1ac3fc0fc382c1f385f91",
+    "url": "/static/media/Mars_Curiosity.ddb438e0.jpg"
+  },
+  {
+    "revision": "3af9b89c174da474a54c0ba06649a9cb",
+    "url": "/static/media/Mars_Opportunity.3af9b89c.jpg"
+  },
+  {
+    "revision": "9d72d1a5f21d11dec8abd084677742cb",
+    "url": "/static/media/Mars_Spirit.9d72d1a5.jpg"
+  },
+  {
+    "revision": "dc58b540c004e3224c91",
+    "url": "/static/css/1.f45e2075.chunk.css"
+  },
+  {
+    "revision": "3bc01e33ffb0ebb406a02d6e75998269",
+    "url": "/index.html"
+  }
+];
