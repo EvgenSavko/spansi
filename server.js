5814554afcaa4238b58f604443c8f69bc8342194
senyaq <34502859+senyaq@users.noreply.github.com>
2019-04-11 15:54:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT !!!77' });
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT !!!177' });
 });
