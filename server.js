993cc7177fee124236e70240a9da4018bc048230
senyaq <34502859+senyaq@users.noreply.github.com>
2019-04-11 09:47:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,25 +3,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'react_project/build')));
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/react_project/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('react_project/build'));
 
-
-// if (process.env.NODE_ENV === 'production') {
-//   // Exprees will serve up production assets
-//   app.use(express.static('react_project/build'));
-
-//   // Express serve up index.html file if it doesn't recognize route
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'react_project', 'build', 'index.html'));
-//   });
-// }
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'react_project', 'build', 'index.html'));
+  });
+}
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
