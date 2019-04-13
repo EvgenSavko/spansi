5e647a0b904abbb2feef0d7263e10eed0d2dd86f
senyaq <senyaq@gmail.com>
2019-04-13 19:58:01 +0300

    add mongoose

---
+++ models/Node.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  first_name: { type: String },
+  second_name: { type: String },
+  phone: { type: String },
+});
+
+const Note = mongoose.model('Note', NoteSchema);
