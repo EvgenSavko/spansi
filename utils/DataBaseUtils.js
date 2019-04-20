e3a26f18724d3aa6dba01d0d7ab6e2dfec9893ca
senyaq <senyaq@gmail.com>
2019-04-20 19:31:58 +0300

    MONGOLAB_URI

--- utils/DataBaseUtils.js
+++ utils/DataBaseUtils.js
@@ -8,7 +8,7 @@ export function setUpConnection() {
   console.log(`setUpConnection`);
   // mongoose.connect('mongodb://localhost:27017/notes', { useNewUrlParser: true });
   mongoose.connect(
-    process.env.PROD_MONGODB ||
+    process.env.MONGOLAB_URI ||
       'mongodb+srv://evgen:486279153@cluster0-zjudz.mongodb.net/notes?retryWrites=true',
     {
       useNewUrlParser: true,
